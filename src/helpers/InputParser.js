75cce27dd4998499e9f36d3ff0a13a13436c409b
guesung <gueit214@naver.com>
2024-12-10 14:36:48 +0900

    feat: 입력값 파싱

--- src/helpers/InputParser.js
+++ src/helpers/InputParser.js
@@ -1,3 +1,11 @@
+import { SEPARATOR } from '../lib/constants.js';
+
 export default class InputParser {
-  static parseUserInput(rawUserInput) {}
+  static parseCarNames(rawCarNAmes) {
+    return rawCarNAmes.split(SEPARATOR);
+  }
+
+  static parseTryCount(rawTryCount) {
+    return Number(rawTryCount);
+  }
 }
