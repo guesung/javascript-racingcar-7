393e96fbade3ca2d7b201f8996ae40cdc557551a
guesung <gueit214@naver.com>
2024-12-10 13:57:09 +0900

    feat: 상수 설정

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,12 +1,18 @@
 export const INPUT_MESSAGE = {
   example: '값을 입력해주세요.',
+  carName: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)',
+  tryCount: '시도할 횟수는 몇 회인가요?',
 };
 
 export const OUTPUT_MESSAGE = {
   example: '결과: ',
+  result: '실행 결과',
+  winner: '최종 우승자 : ',
 };
 
 export const ERROR_MESSAGE_DEFAULT = '[ERROR]';
 export const ERROR_MESSAGE = {
   notExample: `${ERROR_MESSAGE_DEFAULT} this is not example!`,
 };
+
+export const SEPARATOR = ',';
