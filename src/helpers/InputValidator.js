4e203c019d71390788313c2346a698768c684b11
guesung <gueit214@naver.com>
2024-12-10 14:36:53 +0900

    feat: 입력값 유효성 검사

--- src/helpers/InputValidator.js
+++ src/helpers/InputValidator.js
@@ -1,3 +1,27 @@
+import { ERROR_MESSAGE } from '../lib/constants.js';
+
 export default class InputValidator {
-  static validateUserInput(userInput) {}
+  static validateCarNames(cars) {
+    this.#validateCarNameLength(cars);
+    this.#validateCarLength(cars);
+  }
+
+  static #validateCarNameLength(cars) {
+    cars.forEach((car) => {
+      if (car.length === 0 || car.length > 5) throw new Error(ERROR_MESSAGE.carNameLength);
+    });
+  }
+
+  static #validateCarLength(cars) {
+    if (cars.length === 0) throw new Error(ERROR_MESSAGE.carLength);
+  }
+
+  static validateTryCount(tryCount) {
+    this.#validatePositiveInteger(tryCount);
+  }
+
+  static #validatePositiveInteger(number) {
+    if (!Number.isNaN(number) && number > 0 && Number.isInteger(number)) return;
+    throw new Error(ERROR_MESSAGE.positiveInteger);
+  }
 }
