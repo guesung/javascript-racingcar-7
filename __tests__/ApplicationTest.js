a44a07c0f4bacd820bac74a9d3b810489f6c2b8b
guesung <gueit214@naver.com>
2024-12-10 14:01:21 +0900

    test: 예외 테스트 케이스 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -2,36 +2,79 @@ import App from '../src/App.js';
 import { getLogSpy, mockQuestions, mockRandoms } from '../src/lib/testUtils.js';
 
 describe('자동차 경주', () => {
-  test('기능 테스트', async () => {
-    // given
-    const MOVING_FORWARD = 4;
-    const STOP = 3;
-    const inputs = ['pobi,woni', '1'];
-    const logs = ['pobi : -', 'woni : ', '최종 우승자 : pobi'];
-    const logSpy = getLogSpy();
+  describe('기능 테스트', () => {
+    test('', async () => {
+      const MOVING_FORWARD = 4;
+      const STOP = 3;
+      const inputs = ['pobi,woni', '1'];
+      const logs = ['pobi : -', 'woni : ', '최종 우승자 : pobi'];
+      const logSpy = getLogSpy();
 
-    mockQuestions(inputs);
-    mockRandoms([MOVING_FORWARD, STOP]);
+      mockQuestions(inputs);
+      mockRandoms([MOVING_FORWARD, STOP]);
 
-    // when
-    const app = new App();
-    await app.run();
+      const app = new App();
+      await app.run();
 
-    // then
-    logs.forEach((log) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+      logs.forEach((log) => {
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(log));
+      });
     });
   });
 
-  test('예외 테스트', async () => {
-    // given
-    const inputs = ['pobi,javaji'];
-    mockQuestions(inputs);
+  describe('예외 테스트', () => {
+    test('5글자 이상을 입력할 경우 예외를 처리한다.', async () => {
+      const inputs = ['pobi,javaji'];
+      mockQuestions(inputs);
 
-    // when
-    const app = new App();
+      const app = new App();
 
-    // then
-    await expect(app.run()).rejects.toThrow('[ERROR]');
+      await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+
+    test('0글자를 입력할 경우 예외를 처리한다.', async () => {
+      const inputs = [',javaji'];
+      mockQuestions(inputs);
+
+      const app = new App();
+
+      await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+
+    test('0개를 입력할 경우 예외를 처리한다.', async () => {
+      const inputs = [''];
+      mockQuestions(inputs);
+
+      const app = new App();
+
+      await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+
+    test('시도 횟수에 0을 입력한 경우 예외를 처리한다.', async () => {
+      const inputs = ['pobi,woni', 0];
+      mockQuestions(inputs);
+
+      const app = new App();
+
+      await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+
+    test('시도 횟수에 음수를 입력한 경우 예외를 처리한다.', async () => {
+      const inputs = ['pobi,woni', 0];
+      mockQuestions(inputs);
+
+      const app = new App();
+
+      await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
+
+    test('시도 횟수에 소수를 입력한 경우 예외를 처리한다.', async () => {
+      const inputs = ['pobi,woni', 1.1];
+      mockQuestions(inputs);
+
+      const app = new App();
+
+      await expect(app.run()).rejects.toThrow('[ERROR]');
+    });
   });
 });
